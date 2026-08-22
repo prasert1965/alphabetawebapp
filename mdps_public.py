7ef22625846cb78de7739fd6f734b28661eb6e22
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-22 22:32:47 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -55,7 +55,9 @@ now_thai = datetime.now(tz_thai)
 current_time_str = now_thai.strftime("%d/%m/%y time %H:%M minute.")
 
 #loaded_model = pickle.load(open('EAtrained_model.sav', 'rb'))
-
+filename = 'alphabetatrained_model.sav'
+#pickle.dump(log_reg, open(filename, 'wb'))
+pickle.dump(adaboost, open(filename, 'wb'))
 loaded_model = pickle.load(open('alphabetatrained_model.sav', 'rb'))
 
 # giving a title  

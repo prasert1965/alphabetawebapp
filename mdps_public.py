0f1b229a0acc3497248ae353b95146ffa8492c7f
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-23 00:41:49 +0700

    Refactor DB management and Streamlit app structure
    Refactored the DB management and user authentication code, reorganized the prediction logic, and improved the structure of the Streamlit application.

--- mdps_public.py
+++ mdps_public.py
@@ -59,24 +59,6 @@ filename = 'alphabetatrained_model.sav'
 
 loaded_model = pickle.load(open('alphabetatrained_model.sav', 'rb'))
 
-def EA_Alpha_thal_prediction(input_data):
-  
-    # changing the input_data to numpy array
-     input_data_as_numpy_array = np.asarray(input_data)
-
-    # reshape the array as we are predicting for one instance
-     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-     prediction = loaded_model.predict(input_data_reshaped)
-     print(input_data_as_numpy_array)
-     print(prediction)
-
-     if (prediction[0] == 0):
-      return 'This person is alpha thalassemia carrier'
-     else:
-       return 'This person is beta thalassemia carrier'
-  
-
 # giving a title  
 	# st.title('Web for prediction Alpha Thalassemia carrier')   
 def main():
@@ -189,7 +171,22 @@ def main():
 		   add_userdata(new_user,make_hashes(new_passwd))
 		   st.success("You have successfully created an account.Go to the Login Menu to login")
 
+def EA_Alpha_thal_prediction(input_data):
+  
+    # changing the input_data to numpy array
+     input_data_as_numpy_array = np.asarray(input_data)
 
+    # reshape the array as we are predicting for one instance
+     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+     prediction = loaded_model.predict(input_data_reshaped)
+     print(input_data_as_numpy_array)
+     print(prediction)
+
+     if (prediction[0] == 0):
+      return 'This person is alpha thalassemia carrier'
+     else:
+       return 'This person is beta thalassemia carrier'
 
 # giving a title  
 #	st.title('Web for prediction Alpha Thalassemia carrier')   

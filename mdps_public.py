41bc78b2da12f9a89e9ac717fb2e5f2c040ef926
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-22 20:28:36 +0700

    Refactor DB management and prediction logic

--- mdps_public.py
+++ mdps_public.py
@@ -41,7 +41,7 @@ import joblib
 import sklearn
 from sklearn.preprocessing import StandardScaler
 from datetime import datetime
-#scaler = joblib.load('scaler.pkl')
+Scaler= StandardScaler()
 #hide_style = """
 #    <style>
 #    #GithubIcon {visibility: hidden;}

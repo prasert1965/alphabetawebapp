58d456d89d41e0ac0fba15b3759a53fce1cd08a9
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-23 00:56:16 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -58,7 +58,7 @@ current_time_str = now_thai.strftime("%d/%m/%y time %H:%M minute.")
 filename = 'alphabetatrained_model.sav'
 
 with open('alphabetatrained_model.sav', 'rb') as f:
-loaded_model = pickle.load(open('alphabetatrained_model.sav', 'rb'))
+#loaded_model = pickle.load(open('alphabetatrained_model.sav', 'rb'))
 scaler = pickle.load(f)
 
 	# giving a title  

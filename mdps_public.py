b16ec3b3d305c46f02a0bfc7db22618d501b8a74
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-23 01:32:23 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -175,23 +175,22 @@ def main():
 def EA_Alpha_thal_prediction(input_data):
   
     # changing the input_data to numpy array
-	 input_data_as_numpy_array = np.asarray(input_data)
+	input_data_as_numpy_array = np.asarray(input_data)
 
     # reshape the array as we are predicting for one instance
     # input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 	 
-	 scaled_data = scaler.transform(input_data_as_numpy_array)
+	scaled_data = scaler.transform(input_data_as_numpy_array)
      
-	 prediction = loaded_model.predict(scaled_data)
-     
-     print(input_data_as_numpy_array)
-	 print(scaled_data)
-     print(prediction)
+	prediction = loaded_model.predict(scaled_data)
+    print(input_data_as_numpy_array)
+	print(scaled_data)
+    print(prediction)
 
-     if (prediction[0] == 0):
-      return 'This person is alpha thalassemia carrier'
-     else:
-       return 'This person is beta thalassemia carrier'
+    if (prediction[0] == 0):
+     return 'This person is alpha thalassemia carrier'
+    else:
+      return 'This person is beta thalassemia carrier'
 
 # giving a title  
 #	st.title('Web for prediction Alpha Thalassemia carrier')   

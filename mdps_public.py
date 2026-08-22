bbf5de0c170e6525c5f3b37da3d708aa0c0af0c9
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-23 03:39:13 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -180,9 +180,8 @@ def EA_Alpha_thal_prediction(input_data):
 	# reshape the array as we are predicting for one instance
 	input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 	
-	def prepare_data():
-    scaler = StandardScaler()
-    return scaler.fit_transform(input_data_reshaped), scaler
+	scaler = StandardScaler()
+    scaler.fit_transform(input_data_reshaped)
 	
 	scaled_data = scaler.transform(input_data_reshaped)
 	prediction = loaded_model.predict(scaled_data)

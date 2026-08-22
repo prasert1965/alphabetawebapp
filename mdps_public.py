54e4920e3aca20c1f62d2d24f9b471aa340e786b
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-23 02:35:02 +0700

    Refactor user authentication and prediction logic
    Refactor database management and user authentication functions. Update prediction logic for alpha and beta thalassemia.

--- mdps_public.py
+++ mdps_public.py
@@ -180,7 +180,7 @@ def EA_Alpha_thal_prediction(input_data):
 
 	# reshape the array as we are predicting for one instance
 	input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-	scaled_data = scaler.transform(input_data_reshaped)
+	scaled_data = scaler.fit_transform(input_data_reshaped)
 	prediction = loaded_model.predict(input_data_reshaped)
 	
 	print(input_data_as_numpy_array)

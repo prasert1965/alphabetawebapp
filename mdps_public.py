b0ea55a07d5dc13a06fca66fbaa4d42b057f92a4
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-23 20:26:44 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -134,7 +134,7 @@ def main():
 						mchc = float(MCHC) if MCHC else 0.0
 						rdw = float(RDW) if RDW else 0.0
 						hbaii = float(HBAII) if HBAII else 0.0
-						 st.success("")
+					  st.success("")
 					except ValueError:
   						st.error("Please enter a valid number.")
 					

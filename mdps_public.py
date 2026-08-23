4294dbbd594706e8af13b9834b6135557de30b3a
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-23 19:24:59 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -191,7 +191,7 @@ def EA_Alpha_thal_prediction(input_data):
      if (prediction[0] == 0):
       return 'This person is alpha thalassemia carrier'
      else:
-        return 'This person is not alpha thalassemia carrier'
+        return 'This person is beta thalassemia carrier'
 
 
 # giving a title  
